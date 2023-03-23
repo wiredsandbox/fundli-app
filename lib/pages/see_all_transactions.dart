8cbe2af7a0aa30524fedb589d34edf053f100c8e
SpielerEinzig <SpielerEinzig@gmail.com>
2023-03-23 19:49:05 +0100

    Bug: network error

---
+++ lib/pages/see_all_transactions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllTransactionsPage extends StatefulWidget {
+  const AllTransactionsPage({Key? key}) : super(key: key);
+
+  @override
+  State<AllTransactionsPage> createState() => _AllTransactionsPageState();
+}
+
+class _AllTransactionsPageState extends State<AllTransactionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
